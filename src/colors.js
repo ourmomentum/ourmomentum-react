6b92651d7792788c84c6f79ecac1099cc6b5e016
Efe <2021akinciefey@aaps.k12.mi.us>
2020-05-30 23:30:57 +0300

    Added colors.js to import colors

---
+++ src/colors.js
@@ -0,0 +1,34 @@
+export const MOMENTUM_DEEPBLUE = "#081F25";
+
+
+export const MOMENTUM_MEDBLUE = "#51AF9D";
+
+
+export const MOMENTUM_LIGHTBLUE = "#7CCEB7";
+
+
+export const MOMENTUM_YELLOW = "#FCD55E";
+
+
+export const MOMENTUM_ORANGE = "#F5A711";
+
+
+export const MOMENTUM_RED = "#EA421B";
+
+
+export const BG_MOMENTUM_DEEPBLUE = "#081F25";
+
+
+export const BG_MOMENTUM_MEDBLUE = "#51AF9D";
+
+
+export const BG_MOMENTUM_LIGHTBLUE = "#7CCEB7";
+
+
+export const BG_MOMENTUM_YELLOW = "#FCD55E";
+
+
+export const BG_MOMENTUM_ORANGE = "#F5A711";
+
+
+export const BG_MOMENTUM_RED = "#EA421B";
--- src/components/home-screen/Homescreen.js
+++ src/components/home-screen/Homescreen.js
@@ -1,9 +1,16 @@
 import React from 'react'
+import { AppBar, Toolbar,  Typography } from '@material-ui/core'
 
 export default function HomeScreen() {
     return (
         <div>
-            This will eventually be the home screen.
+            <AppBar style={{background: "#FFFFFF"}}>
+                <Toolbar style={{color: "black"}}>
+                    <Typography>
+                        Stuff will hopefully go here
+                    </Typography>
+                </Toolbar>
+            </AppBar>
         </div>
     )
 }
