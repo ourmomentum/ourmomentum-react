b09ad99809074bd562d2e21cb160115b78bc8bf5
muhsin7 <muhsin.ma.mohamed@gmail.com>
2020-05-31 01:15:02 +0530

    added navbar + banner in homepage
    - couldnt align navbar links to right (try flex perhaps?)
    - couldnt center banner with grid so i just used a center tag ¯\_(ツ)_/¯

--- src/components/home-screen/Homescreen.js
+++ src/components/home-screen/Homescreen.js
@@ -1,9 +1,53 @@
 import React from 'react'
+import './Homescreen.css'
+import {
+  Container,
+  AppBar,
+  Toolbar,
+  Button,
+  Grid
+} from '@material-ui/core'
 
 export default function HomeScreen() {
     return (
-        <div>
-            This will eventually be the home screen.
+      <>
+      <AppBar position="static" color="transperant" class="navbar">
+        <Toolbar>
+        <img src="logosvg.svg" height="75px" />
+        <div class="navbar_links">
+          <Button color="inherit">Home</Button>
+          <Button color="inherit">Issues</Button>
+          <Button color="inherit">Blog</Button>
+          <Button color="inherit">Our Team</Button>
+          <Button color="inherit">Contact Us</Button>
         </div>
+        </Toolbar>
+        </AppBar>
+
+        <center>
+          <img src="homepage_banner.png" class="banner"/>
+        </center>
+
+        {/* style tests for "euerka" heading
+        <Container>
+          <center>
+            <div class="edition0">
+              EUREKA
+            </div>
+            <div class="edition">
+              EUREKA
+            </div>
+
+            <div class="edition2">
+              EUREKA
+            </div>
+            <div class="edition3">
+              EUREKA
+            </div>
+          </center>
+        </Container>
+          */}
+        </>
+
     )
 }
