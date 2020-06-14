fa8f9a33c3c03546f8270dd2b2ed1d288db47530
Susan <2021xisusans@aaps.k12.mi.us>
2020-06-13 22:15:18 -0400

    susans playground is updated and accessible thru localhost:3000/susans-playground

--- src/components/susans-playground/SusansPlayground.js
+++ src/components/susans-playground/SusansPlayground.js
@@ -2,22 +2,70 @@ import React from 'react'
 import './SusansPlayground.css'
 import {Grid} from '@material-ui/core';
 import ReactDOM from 'react-dom'
+import PropTypes from 'prop-types';
+import { makeStyles } from '@material-ui/core/styles';
+import Container from '@material-ui/core/Container';
+import Typography from '@material-ui/core/Typography';
+import Link from '@material-ui/core/Link';
+
+function Copyright() {
+  return (
+    <Typography variant="body2" color="textSecondary" align="center">
+      {'Copyright © SusansPlayground '}
+      <br />
+      <Link color="inherit" href="https://youtu.be/dQw4w9WgXcQ">
+        My Onlyfans
+      </Link>{' '}
+      {new Date().getFullYear()}
+      {'.'}
+    </Typography>
+  );
+}
+
+const useStyles = makeStyles((theme) => ({
+  footer: {
+    backgroundColor: theme.palette.background.paper,
+    // marginTop: theme.spacing(8),
+    padding: theme.spacing(6, 0),
+  },
+}));
 
 
- export default function SusansPlayground() {
+
+
+ export default function SusansPlayground(props) {
+
+const classes = useStyles();
+const { description, title } = props;
+
+
     return (
-       <Grid container style = {{height: "100vh", width: "100vw", display: "flex", alignItems: "center"}}>
+       <Grid >
+         <Grid container style = {{height: "100vh", width: "100vw", display: "flex", alignItems: "center"}}>
             <Grid item xs/>
-                <Grid id="loadingImageContainer" item xs = {5} md={1}>
-                    <img alt="Loading..." src = "/logosvg.svg" style = {{height: "auto"}}/>   
-                
-                </Grid>
-            <Grid item xs/>
-            <p>
+            <p align = "center">
             fucking fucker
             </p>
-        </Grid>
-    
+           <Grid item xs/>
+
+         </Grid>
+
+         <footer className={classes.footer}>
+        
+          <Container maxWidth="lg">
+            <Typography variant="h6" align="center" gutterBottom>
+              {"so much code for so little"}
+            </Typography>
+            <Typography variant="subtitle1" align="center" color="textSecondary" component="p">
+              {"teeeeny tiny text"}
+            </Typography>
+            <Copyright />
+            
+          </Container>
+         </footer>
+
+        
+    </Grid>
     
     
 
