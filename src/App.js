b7b5f5554c2766372fdc5142d0d8fcdb6ad4515e
muhsin7 <muhsin.ma.mohamed@gmail.com>
2020-06-18 00:39:30 +0530

    testposts moved to db
    posts can now be accessed with ReturnListOfPosts() from testposts.js

--- src/App.js
+++ src/App.js
@@ -3,21 +3,20 @@ import './App.css';
 import { BrowserRouter, Route } from "react-router-dom";
 import SusansPlayground from './components/susans-playground/SusansPlayground';
 import Landing from './components/Landing';
+import firebase from './components/home-screen/Firebase';
 
 
 
 function App() {
-
   return (
     <BrowserRouter>
 
       <Route path = "/susans-playground" component = {SusansPlayground}>
-
       </Route>
       <Route exact path = "/" component = {Landing}>
 
       </Route>
-      
+
     </BrowserRouter>
   );
 }
--- src/components/home-screen/FeaturesArticles.js
+++ src/components/home-screen/FeaturesArticles.js
@@ -1,6 +1,7 @@
 import React from 'react'
-import posts from './testposts'
-import testposts from './testposts';
+import posts from './Testposts.js'
+// import testposts from './testposts';
+import ReturnListOfPosts from './Testposts.js'
 import { Grid, Typography } from '@material-ui/core'
 import BlogPreview from '../../blog-preview/BlogPreview'
 import { useTheme } from '@material-ui/core/styles';
@@ -10,14 +11,14 @@ export default function FeaturesArticles() {
     const theme = useTheme();
 
     return (
-        <div>    
+        <div>
             <Grid container>
                 <Grid item xs={12} style={{display: 'flex', justifyContent: 'center', paddingTop:'1.5em', paddingBottom:'0em'}}>
                     <Typography variant='h3' style={{fontWeight:'bold', letterSpacing: '0.25em'}}>FEATURED ARTICLES</Typography>
                 </Grid>
             </Grid>
                 <Grid container style={{width: '100%', marginTop: '2.5vh'}}>
-                    {testposts.map((post, i) => <BlogPreview margin = {((!theme.breakpoints.down('sm') ? 1 : (2 % 2) ) + 2 + 'em')} title={post.title}  content={post.content} />)}
+                    {ReturnListOfPosts.forEach().map((post, i) => <BlogPreview margin = {((!theme.breakpoints.down('sm') ? 1 : (2 % 2) ) + 2 + 'em')} title={post.title}  content={post.content} />)}
                 </Grid>
         </div>
     )
