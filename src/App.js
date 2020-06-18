98ddbc94a0be13eabf071724a73ce30664f8719c
muhsin7 <muhsin.ma.mohamed@gmail.com>
2020-06-18 23:18:26 +0530

    pushing routes for it to be merged away :PepeHands:

--- src/App.js
+++ src/App.js
@@ -2,11 +2,19 @@ import React from 'react';
 import './App.css';
 import { BrowserRouter, Route } from 'react-router-dom';
 import Landing from './components/home-screen/Landing';
+import Issues from './components/pages/Issues'
+import Blog from './components/pages/Blog'
+import Team from './components/pages/Team'
+import Contact from './components/pages/Contact'
 
 function App() {
   return (
     <BrowserRouter>
       <Route exact path = '/' component={Landing} />
+      <Route path="/issues" component={Issues} />
+      <Route path="/blog" component={Blog} />
+      <Route path="/team" component={Team} />
+      <Route path="/contact" component={Contact} />
     </BrowserRouter>
   );
 }
