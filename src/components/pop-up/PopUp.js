edd6ead22a7f72d33ea1599b4ecaf6fec60124a4
Efe <2021akinciefey@aaps.k12.mi.us>
2020-07-06 04:18:06 +0300

    Pop up now uses the Dialog comp.

--- src/components/pop-up/PopUp.js
+++ src/components/pop-up/PopUp.js
@@ -1,30 +1,33 @@
 import React, { Component } from "react";
-import { Button, Typography } from '@material-ui/core';
+import { Button, Typography, Dialog, DialogTitle, DialogContent, DialogContentText, TextField, DialogActions } from '@material-ui/core';
 import CloseIcon from '@material-ui/icons/Close';
 
-export default class PopUp extends Component {
-    handleClick = () => {
-        this.props.toggle();
-    };
-    
-    render() {
-        return (
-            <div style={{right:'0%', bottom:'0%', position:'fixed',  width:'100%', height:'100%', backgroundColor:'rgba(0, 0, 0, 0.6)', zIndex:'1'}}>
-                <div style={{width:'100vh', height:'auto', transform:'translate(-50%, -50%)', justifyContent: 'flex-end', top:'50%', left:'50%', backgroundColor:'white', position:'fixed', padding:'1em', border:'3px solid black'}}>
-                    <span style={{color:'black', float:'rich'}} onClick={this.handleClick}><CloseIcon /></span>
-                    <Typography variant='h4' style={{fontWeight:'bold', display: 'flex', justifyContent: 'center'}} >FOLLOW US</Typography>
-                    <Typography style={{paddingLeft:'1em', paddingTop:'1em', paddingBottom:'1em'}}>
-                        Want to stay updated with what we're up to? Enter your email below address and receive notifications of new posts by email. 
-                    </Typography>
-                    <div style={{marginLeft:'50%', transform:'translateX(-50%)'}}>
-                        <form>
-                            <input type="email" name="email" style={{fontSize:'1em', width:'21em', marginLeft:'50%', transform:'translateX(-50%)'}}/>
-                            <br />
-                            <Button variant='outlined' style={{fontSize:'1.1em', marginTop:'0.95em', marginRight:'1em', backgroundColor:'#51AF9D', color:'white', borderColor:'#51AF9D', marginLeft:'50%', transform:'translateX(-50%)'}}>Submit</Button>
-                        </form>
-                    </div>
-                </div>
-            </div>
-        );
+export default function PopUp(props) {
+    const handleSubscribe = () => {
+        props.closePopUp();
     }
+        return (
+                <Dialog open={props.open} onClose={props.closePopUp}>
+                    <DialogTitle>Subscribe</DialogTitle>
+                    <DialogContent>
+                    <DialogContentText>
+                        Want to stay updated with what we're up to? Enter your email below address and receive notifications of new posts by email. 
+                    </DialogContentText>
+                    <TextField
+                        autoFocus
+                        label="Email Address"
+                        type="email"
+                        fullWidth
+                    />
+                    </DialogContent>
+                    <DialogActions>
+                    <Button onClick={props.closePopUp} color="primary">
+                        Cancel
+                    </Button>
+                    <Button onClick= {handleSubscribe} color="primary">
+                        Yes
+                    </Button>
+                    </DialogActions>
+                </Dialog>
+        );
 }
--- src/components/pop-up/PoppingUp.js
+++ src/components/pop-up/PoppingUp.js
@@ -1,26 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 import PopUp from "./PopUp"; 
 import { Button } from '@material-ui/core';
 
-export default class PoppingUp extends React.Component {
-    state = {
-        seen: false
-    };  
-    
-    togglePop = () => {
-        this.setState({
-            seen: !this.state.seen
-        });
-    };
-    
-    render() {
-        return (
-            <div>
-                <div style={{textAlign:'center'}} onClick={this.togglePop}>
-                    <Button variant='outlined' style={{marginRight:'1em', color:'#9c9c9c'}}>Follow Us</Button>
-                </div>
-                {this.state.seen ? <PopUp toggle={this.togglePop} /> : null}
+export default function PoppingUp(props) {
+    const [open, setOpen] = useState(false);
+    return (
+        <div>
+            <div style={{textAlign:'center'}} onClick={setOpen.bind(this, true)}>
+                <Button variant='outlined' style={{marginRight:'1em', color:'#9c9c9c'}}>Follow Us</Button>
             </div>
-        );
-    }
+            <PopUp open={open} closePopUp={setOpen.bind(this, false)} />
+        </div>
+    );
 }
